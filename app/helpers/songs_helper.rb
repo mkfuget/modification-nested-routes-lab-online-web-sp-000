74a3db4d34263486b73c621ae15c2fb3cc3c5ee4
Michael Fuget <michaelfuget@gmail.com>
2021-01-31 03:03:56 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def artist_select(song, in_song)
     if in_song
-      "<%= f.collection_select_boxes :artist_id, Artist.all, :id, :name %>"
+      "<%= f.collection_select :artist_id, Artist.all, :id, :name %>"
     else
       "<%= f.label :artist_name %><%= f.text_field :artist_name %>"
     end
